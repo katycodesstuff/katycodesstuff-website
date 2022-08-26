c6cbc397b1fcf423ed193e458e4096d3b4cc79b8
Katy Ashby <33542984+katycodesstuff@users.noreply.github.com>
2022-08-26 17:06:14 +0100

    Add missing files

---
+++ ui/src/components/Footer/Footer.tsx
@@ -0,0 +1,27 @@
+import './Footer.css'
+
+interface Props {}
+
+function Footer(props: Props) {
+    const {} = props
+
+    return (
+        <footer>
+            <div className='footer-flex-item'>
+            <h1>Katy Ashby</h1>
+            <p>This website was designed and created by Katy using React, TypeScript and hosted on Netlify.</p>
+            <p>©️ KatyCodesStuff - Katy Ashby 2022</p>
+            <p className="footer-attributes"><a href="https://www.flaticon.com/free-icons/uk" title="UK icon">UK icon created by surang - Flaticon</a></p>
+            </div>
+
+            <div className='footer-flex-item'>
+            <h1>Let’s stay in touch!</h1>
+            <p>I’d love to hear from you!<br />Send me an email or send me a DM on Twitter with your request.</p>
+            <p>EMAIL<br /><a href='mailto:katycodesstuff@gmail.com'>katycodesstuff@gmail.com</a></p>
+            <p>TWITTER<br /><a href='https://twitter.com/KatyCodesStuff'>@KatyCodesStuff</a></p>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
