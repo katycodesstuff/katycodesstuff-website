adb8564d934222cf3b16914e65f0c88d13330e0f
Katy Ashby <33542984+katycodesstuff@users.noreply.github.com>
2022-09-27 11:42:53 +0100

    Make a functional menu page with temp icons

--- ui/src/components/NavBar/NavBar.tsx
+++ ui/src/components/NavBar/NavBar.tsx
@@ -1,20 +1,39 @@
+import { useState } from 'react'
 import { NavLink } from 'react-router-dom'
+import NavMenu from '../NavMenu/NavMenu'
 import './NavBar.css'
+
+const navLinks = [
+    { to: '/', title: 'About' },
+    { to: '/partnerships', title: 'Partnerships' }
+]
 
 function NavBar() {
 
+    const [navMenuIsToggled, setNavMenuIsToggled] = useState(false);
+
     return (
         <header>
-            <span className='mouse-icon'>🐭</span>
-            <span className='logo'>KatyCodesStuff</span>
-            
-            <span className='navigation-links'>
-                <NavLink to='/' className={({ isActive }) => (isActive ? 'navigation-links-active' : '')}>About</NavLink>
-                <NavLink to='/partnerships' className={({ isActive }) => (isActive ? 'navigation-links-active' : '')}>Partnerships</NavLink>
-                {/* <NavLink to='/random' className={({ isActive }) => (isActive ? 'navigation-links-active' : '')}>Get in touch</NavLink> */}
+            <div className='header-bar'>
+                <span className='mouse-icon'>🐭</span>
+                <span className='logo'>KatyCodesStuff</span>
+                
+                <span className='navigation-links'>
+                    {navLinks.map((link, i) => <NavLink key={i} to={link.to} className={activePageFunc}>{link.title}</NavLink>)}
                 </span>
+                <span className='navigation-menu-icon' onClick={() => onToggleMenu(setNavMenuIsToggled)}>
+                    🔗
+                </span>
+            </div>
+            <NavMenu links={navLinks} isVisible={navMenuIsToggled} setToggle={setNavMenuIsToggled}></NavMenu>
         </header>
     )
+}
+
+var activePageFunc: ((props: any) => string) = ({isActive}) => (isActive ? 'navigation-links-active' : '')
+
+function onToggleMenu(setNavMenuIsToggled: any) {
+    setNavMenuIsToggled((previousState: boolean) => !previousState)
 }
 
 export default NavBar
